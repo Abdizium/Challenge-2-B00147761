d5126514175ff56bf0c3490f2b55494f9d601233
Abdizium <114268903+Abdizium@users.noreply.github.com>
2022-12-23 23:52:54 +0000

    Challenge-2-B00147761

---
+++ fechproject/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float waitime = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && waitime <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            waitime = 1;
+
+        }
+        if (waitime >= 0) {
+            waitime -= Time.deltaTime;
+        }
+    }
+}
+ 
